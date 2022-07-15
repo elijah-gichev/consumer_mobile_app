54e89b122db41e67e56231c9351e4b345570b444
Vladimir <n143vova@gmail.com>
2022-07-15 19:06:53 +0300

    created details model

---
+++ lib/ui/pages/details_page/details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 760.0.h,
+      width: double.infinity,
+      child: Column(),
+    );
+  }
+}
