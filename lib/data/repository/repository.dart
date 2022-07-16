ec7d0a20efecbb70315582545bfbbbfbc3c67328
boyallex <60341259+boyallex@users.noreply.github.com>
2022-07-17 00:37:25 +0300

    Update repository.dart

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../ui/models/details.dart';
+import '../../ui/models/flats.dart';
 import '../../ui/models/graph.dart';
 
 @singleton
@@ -37,7 +38,7 @@ class Repository {
     );
     return res;
   }
-  Future<List<Graph>> getFilteredFlats(Details data) async {
+  Future<List<Flats>> getFilteredFlats(Details data) async {
     final Response request = await dio.get(
       dom + '/api/flat?cost_from=2500000&cost_to=4500000&room_count=2&height_from=2.5&repair=С ремонтом&floor_count_from=4&floor_count_to=12&view=На улицу&material=Монолитный',
       options: Options(
@@ -47,17 +48,11 @@ class Repository {
       ),
     );
     final Map data = jsonDecode(request.toString());
-    List<Graph> res = [];
-    data.forEach(
-      (key, value) {
-        res.add(
-          Graph(
-            coord: value,
-            year: key,
-          ),
-        );
-      },
-    );
+    final res = data["data"]
+        .map<Flats>(
+          (e) => Flats.fromMap(e),
+        )
+        .toList();
     return res;
   }
 }
